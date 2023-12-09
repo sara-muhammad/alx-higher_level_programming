4c81bdf42678eaa02725334898f6bcb3c5ba0509
sara-muhammad <saraelakaad@gmail.com>
2023-12-09 15:47:19 +0200

    b

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,16 @@
-#!S/usr/bin/python3
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    """program to print square of a matrix
+
+    Args:
+        matrix: the list of the square
+
+    Returns:
+        new matrix or None if null
+    """
     if len(matrix) == 0 or matrix is None:
         return (None)
     new_matrix = []
-    for i in matrix:
-        new_matrix.append([x ** 2 for x in i])
-return (new_matrix)
+    for each in matrix:
+        new_matrix.append([x ** 2 for x in each])
+    return (new_matrix)
